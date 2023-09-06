d664e385af74c35273a443587fc4f0fef367ce1a
smmunna <minhazulabedinmunna@gmail.com>
2023-09-06 23:51:52 +0600

    folder structure created

--- tailwind.config.js
+++ tailwind.config.js
@@ -1,9 +1,26 @@
 /** @type {import('tailwindcss').Config} */
 export default {
-  content: [],
-  theme: {
-    extend: {},
+  content: [
+    "./index.html",
+    "./src/**/*.{js,ts,jsx,tsx}",
+  ],
+  daisyui: {
+    themes: [
+      {
+        mytheme: {
+          "primary": "#88dce2",
+          "secondary": "#e88774",
+          "accent": "#e09c64",
+          "neutral": "#2d233e",
+          "base-100": "#263a4b",
+          "info": "#4295c2",
+          "success": "#4dd5c5",
+          "warning": "#f9bb5d",
+          "error": "#f85549",
+        },
+      },
+    ],
   },
-  plugins: [],
+  plugins: [require("daisyui")],
 }
 
